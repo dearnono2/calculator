aab23d82146d75a0c08880e5bec788105b1e4859
dearnono2 <dearnono2@gmail.com>
2023-06-21 01:43:05 +0900

    임시

--- master-calculator/script.js
+++ master-calculator/script.js
@@ -1,8 +1,8 @@
 const btn = document.querySelectorAll("button");
 
-let prevValue = [];
-let nextValue = [];
-let currentValue = [];
+let prevValue = '';
+let nextValue = '';
+let currentValue;
 
 let cal = "";
 
@@ -24,52 +24,84 @@ btn.forEach((v, i) => {
     }
 
     if (type === "equal") {
-      value;
+      result(value);
     }
   });
 });
 
 function number(v) {
-  cal += v;
-  console.log(cal);
+  if(currentValue === undefined) {
+    currentValue = '';
+  }
+  currentValue += v;
+  
+  console.log(currentValue);
 }
 
 function operator(v) {
-  const checkDoubleOperator = cal[cal.length - 1];
+  const checkDoubleOperator = currentValue[currentValue.length - 1];
+  // 만약 첫 입력이 연산자면? => 무시
+  // 만약 숫자가 입력된 후 연산자를 입력하면? => current 값을 prevValue에 옮겨준다.
+  // 만약 nextValue값이 없다면? => 연산 안함.
+  // 연산자를 연속으로 두번 입력하면? => 가장 최근에 입력한 연산자로 바꿔준다.
+  // 만약 prevValue에 값이 있고 연산자가 입력된다면? 그 후에 입력되는 숫자는 nextValue에 저장
 
-  if (
-    checkDoubleOperator === "+" ||
-    checkDoubleOperator === "-" ||
-    checkDoubleOperator === "*" ||
-    checkDoubleOperator === "/"
-  ) {
-    cal = cal.replace(checkDoubleOperator, v);
-    console.log("연속임");
-  } else if (cal === "") {
-    cal = cal;
-    console.log("연산자가 처음임");
+  if(currentValue === undefined) {
+    console.log('첫입력이 연산자');
   } else {
-    prevValue.push(cal);
-    cal += v;
-    console.log("연속 아님");
+    prevValue = currentValue;
+    if(prevValue !== undefined) {
+      currentValue = prevValue += v;
+      console.log('prev값 있음')
+
+    } else {
+      currentValue += v;
+      console.log('prev값 없음')
+    }
+    if (
+      checkDoubleOperator === "+" ||
+      checkDoubleOperator === "-" ||
+      checkDoubleOperator === "*" ||
+      checkDoubleOperator === "/"
+    ) {
+      currentValue = currentValue.replace(checkDoubleOperator, v);
+      console.log("연속임");
+    
+    } else {
+
+
+      switch (v) {
+        case "+":
+          prevValue = currentValue + nextValue;
+          break;
+        case "-":
+          prevValue = currentValue - nextValue;
+          break;
+        case "*":
+          prevValue = currentValue * nextValue;
+          break;
+        case "/":
+          prevValue = currentValue / nextValue;
+          break;
+      }
+    }
+
+    console.log(currentValue);
   }
 
-  console.log(cal);
+
+
+  // console.log(cal);
 
   // 연산자가 들어오면 즉시, 지금까지 입력되었던 숫자값이 prevValue로 들어감.
   // 만약 바로 앞 문자가 연산자라면 연속되는 두번째 연산자는 무시
-  switch (v) {
-    case "+":
-      prevValue = prevValue[0] + v;
-    case "-":
-      prevValue = prevValue[0] - v;
-    case "*":
-      prevValue = prevValue[0] * v;
-    case "/":
-      prevValue = prevValue[0] / v;
-  }
+
 }
 
 function result(v) {
-  prevValue = resultValue;
+  if(prevValue !== undefined) {
+    prevValue = eval(currentValue);
+
+  }
+  console.log(prevValue);
 }
