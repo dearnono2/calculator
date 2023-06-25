2eee4100067f07b8c9081c66b32dc663f5ff5961
dearnono2 <dearnono2@gmail.com>
2023-06-25 23:59:05 +0900

    numBtn부분 로직 수정 필요

--- calculator-last-of-last/script.js
+++ calculator-last-of-last/script.js
@@ -18,18 +18,29 @@ screen.textContent = 0;
 
 numBtn.forEach((v) => {
   v.addEventListener("click", (e) => {
+    // 입력 되어있는 마지막 값이 숫자인가?
+    let isNum = Number(screen.textContent[screen.textContent.length - 1]);
+    console.log(typeof(isNum) === "string");
     prevValue += e.target.value;
 
     // 0다음에 자연수 못오게 하기 && 숫자 다음에 0은 가능.
+    //  이부분 로직 헷갈린다 ㅠㅠㅠㅠㅠㅠ
     if (
-      screen.textContent[screen.textContent.length - 1] > 0 ||
-      typeof screen.textContent[screen.textContent.length - 1] !== "number"
+      // 입력 되어있는 마지막 값이 자연수(숫자 다음에 0인 경우임.)
+      screen.textContent[screen.textContent.length - 1] > 0 &&
+      // 입력 되어있는 마지막 값이 "number"라는 string이 아님
+      isNum !== "number" ||
+      e.target.value !== "00"
     ) {
+
       screen.textContent += e.target.value;
+      console.log('1작동')
     }
-    if (screen.textContent[0] === "0") {
+    // 만약 00이 입력되었을 경우 작동되면 안됨.
+    if (screen.textContent[0] === "0" && e.target.value !== "00") {
       screen.textContent = "";
       screen.textContent += e.target.value;
+      console.log('2작동')
     } else {
       if (screen.textContent[screen.textContent.length - 1] === "0") {
         console.log("its 0");
@@ -38,10 +49,17 @@ numBtn.forEach((v) => {
   });
 });
 
+// operators
 operBtn.forEach((v) => {
   v.addEventListener("click", (e) => {
     console.log(e.target.value);
-    screen.textContent += e.target.value;
+
+    if(screen.textContent[screen.textContent.length - 1] === "+" || screen.textContent[screen.textContent.length - 1] === "-" || screen.textContent[screen.textContent.length - 1] === "/" || screen.textContent[screen.textContent.length - 1] === "*") {
+      return;
+    } else {
+      screen.textContent += e.target.value;
+
+    }
   });
 });
 
@@ -75,9 +93,14 @@ clear.addEventListener("click", (e) => {
 //  decimal point
 decimal.addEventListener("click", (e) => {
   console.log(e.target.value);
-  screen.textContent += e.target.value;
+  if(screen.textContent[screen.textContent.length - 1] === "." || screen.textContent[screen.textContent.length - 1] === "+" || screen.textContent[screen.textContent.length - 1] === "-" || screen.textContent[screen.textContent.length - 1] === "/" || screen.textContent[screen.textContent.length - 1] === "*") {
+    return;
+  } else {
+    screen.textContent += e.target.value;
+  }
 });
 
+// result
 equal.addEventListener("click", (e) => {
   console.log(e.target.value);
   screen.textContent = eval(screen.textContent);
